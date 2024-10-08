ca003a410d69e2bb1ed0399b7cb5cb97961e42c4
MorganRM <oihan.rm@gmail.com>
2024-10-08 11:36:13 +0200

    Refactor code and add AlumnadoService to app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,12 @@ export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Almi', url: '/folder', icon: 'home' },
   ];
+  public selectedIndex: number | undefined;
+
   constructor(private platform:Platform ) {
     this.initializeApp();
-  }
+    this.selectedIndex = 0;
+    }
   initializeApp() {
     this.platform.ready().then(() => {
       console.log('Platform ready');
@@ -21,5 +24,9 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     console.log('App Component');
+    const path=window.location.pathname.split('folder/')[1];
+    if (path!== undefined){
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AlumnadoService } from './servicios/alumnado.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-  ],
+  providers: [ AlumnadoService,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
